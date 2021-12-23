da51572b3bf9bcd16b8461e01254626887efae65
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2021-12-22 23:24:44 -0800

    change a pixel to a color

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import json
 import ast
+
+X_NAME = 'x'
+Y_NAME = 'y'
+RED_NAME = 'r'
+GREEN_NAME = 'g'
+BLUE_NAME = 'b'
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -11,6 +18,31 @@ class Pixels(Resource):
         with open('canvas.json', 'r') as f:
             data = json.load(f)
         return {'data':data}, 200
+    
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+
+        parser.add_argument(X_NAME, required=True)  # add args
+        parser.add_argument(Y_NAME, required=True)
+        parser.add_argument(RED_NAME,required=True)
+        parser.add_argument(GREEN_NAME,required=True)
+        parser.add_argument(BLUE_NAME,required=True)
+        
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # read our json
+        with open('canvas.json', 'r') as f:
+            data = json.load(f)
+        # add the newly provided values
+        x = int(args[X_NAME])
+        y = int(args[Y_NAME])
+        color = [int(args[RED_NAME]), int(args[GREEN_NAME]), int(args[BLUE_NAME])]
+        data[x][y]=color
+        # save back
+        with open('canvas.json', 'w', newline='') as f:
+           json.dump(data, f, separators=(',',':'))
+
+        return {'data': data}, 200  # return data with 200 OK
     pass
 
 api.add_resource(Pixels, '/pixels') # entry point for pixels
