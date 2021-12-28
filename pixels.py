a1eb88ce9034f5a05a666ecc0cea40e9c53fe03a
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2021-12-28 15:36:19 -0800

    fix json in other place

--- pixels.py
+++ pixels.py
@@ -35,24 +35,21 @@ class Pixels(Resource):
         canvas[x][y]=color
         # save back
         with open('canvas.json', 'w', newline='') as f:
-            f.write("{\"data\":{")
+            f.write("{\"canvas\":[{")
             #json.dump(data, f, separators=(',',':'))
-            f.write("\"pixels\":{")
-            f.write("\"pixel\":{\"r\":"+str(canvas[0][0][0])+",\"g\":"+str(canvas[0][0][1])+",\"b\":"+str(canvas[0][0][2]))
+            f.write("\"px_row\":[{\"r\":"+str(canvas[0][0][0])+",\"g\":"+str(canvas[0][0][1])+",\"b\":"+str(canvas[0][0][2]))
             for j in range(1, len(canvas[0])):
                 f.write("},") 
-                f.write("\"pixel\":{\"r\":"+str(canvas[0][j][0])+",\"g\":"+str(canvas[0][j][1])+",\"b\":"+str(canvas[0][j][2]))
-            f.write("}")
+                f.write("{\"r\":"+str(canvas[0][j][0])+",\"g\":"+str(canvas[0][j][1])+",\"b\":"+str(canvas[0][j][2]))
             for i in range(1,len(canvas)):
-                f.write("},\"pixels\":{")
-                f.write("\"pixel\":{\"r\":"+str(canvas[i][0][0])+",\"g\":"+str(canvas[i][0][1])+",\"b\":"+str(canvas[i][0][2]))
+                f.write("}]},")
+                f.write("{\"px_row\":[{\"r\":"+str(canvas[i][0][0])+",\"g\":"+str(canvas[i][0][1])+",\"b\":"+str(canvas[i][0][2]))
                 for k in range(1,len(canvas[i])):
                     f.write("},") 
-                    f.write("\"pixel\":{\"r\":"+str(canvas[i][k][0])+",\"g\":"+str(canvas[i][k][1])+",\"b\":"+str(canvas[i][k][2]))
-                f.write("}")
+                    f.write("{\"r\":"+str(canvas[i][k][0])+",\"g\":"+str(canvas[i][k][1])+",\"b\":"+str(canvas[i][k][2]))
             f.write("}")
-            f.write("}")
-            f.write("}")
+            f.write("]}")
+            f.write("]}")
 
         return canvas, 200  # return data with 200 OK
     pass
