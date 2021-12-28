ed1d7d9e1cd83871334f781df230a2aa6f85a97c
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2021-12-28 15:34:44 -0800

    fixed json for realsies

--- canvas.py
+++ canvas.py
@@ -15,21 +15,18 @@ for j in range(NUM_COL):
 
 # write to json file with own encoding
 with open('canvas.json', 'w', newline='') as f:
-    f.write("{\"data\":{")
+    f.write("{\"canvas\":[{")
     #json.dump(data, f, separators=(',',':'))
-    f.write("\"p_row0\":{")
-    f.write("\"p_"+str(0)+"_"+str(0)+"\":{\"r\":"+str(canvas[0][0][0])+",\"g\":"+str(canvas[0][0][1])+",\"b\":"+str(canvas[0][0][2]))
+    f.write("\"px_row\":[{\"r\":"+str(canvas[0][0][0])+",\"g\":"+str(canvas[0][0][1])+",\"b\":"+str(canvas[0][0][2]))
     for j in range(1, len(canvas[0])):
         f.write("},") 
-        f.write("\"p_"+str(0)+"_"+str(j)+"\":{\"r\":"+str(canvas[0][j][0])+",\"g\":"+str(canvas[0][j][1])+",\"b\":"+str(canvas[0][j][2]))
-    f.write("}")
+        f.write("{\"r\":"+str(canvas[0][j][0])+",\"g\":"+str(canvas[0][j][1])+",\"b\":"+str(canvas[0][j][2]))
     for i in range(1,len(canvas)):
-        f.write("},\"p_row"+str(i)+"\":{")
-        f.write("\"p_"+str(i)+"_"+str(0)+"\":{\"r\":"+str(canvas[i][0][0])+",\"g\":"+str(canvas[i][0][1])+",\"b\":"+str(canvas[i][0][2]))
+        f.write("}]},")
+        f.write("{\"px_row\":[{\"r\":"+str(canvas[i][0][0])+",\"g\":"+str(canvas[i][0][1])+",\"b\":"+str(canvas[i][0][2]))
         for k in range(1,len(canvas[i])):
             f.write("},") 
-            f.write("\"p_"+str(i)+"_"+str(k)+"\":{\"r\":"+str(canvas[i][k][0])+",\"g\":"+str(canvas[i][k][1])+",\"b\":"+str(canvas[i][k][2]))
-        f.write("}")
+            f.write("{\"r\":"+str(canvas[i][k][0])+",\"g\":"+str(canvas[i][k][1])+",\"b\":"+str(canvas[i][k][2]))
     f.write("}")
-    f.write("}")
-    f.write("}")
+    f.write("]}")
+    f.write("]}")
