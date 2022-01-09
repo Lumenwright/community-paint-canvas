e7fb75a24f1ca45cf78e2aa2a78bc6178f33ed2b
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2022-01-09 04:48:42 -0800

    connection to Tiltify and resolving invoice WORKS

--- invoice.py
+++ invoice.py
@@ -1,7 +1,44 @@
 import json
+import requests
+import dont_commit
+import pixels
+
+INVOICE_FILE = 'invoice.json'
+index = 0
+
 def make_invoice(total_donate, response):
-    s = {"total_donate": total_donate, "response": response}
-    with open('invoice.json','w', newline='') as f:
+    s = {response:{"total_donate": total_donate}}
+    with open(INVOICE_FILE,'w', newline='') as f:
         json.dump(s, f)
 
 #resolve invoice
+def resolve_invoice():
+    #get the last 10 donations
+    header = {"Authorization":"Bearer "+dont_commit.AT}
+    r = requests.get("https://tiltify.com/api/v3/campaigns/"+dont_commit.ID+"/donations", headers=header)
+    d = json.loads(r.text)
+    donations = d["data"]
+
+    #compare response to text in invoice
+    with open(INVOICE_FILE, 'r+') as f:
+        invoice = json.load(f)
+        found = False
+        while(not found):
+            for donation in donations:
+                for entry in invoice:
+                    code = donation["comment"]
+                    if(entry==code):
+                        print(code)
+                        found = True
+                        break
+        if(found):
+            invoice.pop(entry)
+            f.seek(0)
+            json.dump(invoice,f)
+            f.truncate()
+        else:
+            print("couldn't find match")
+
+if(__name__ == '__main__'):
+    #make_invoice(1.00, "test")
+    #resolve_invoice()
