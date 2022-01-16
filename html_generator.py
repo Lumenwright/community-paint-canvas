0f16effdaf1703d3942d2f20bd164753a7683e29
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2022-01-16 14:30:42 -0800

    generate a canvas that isn't so intensive to load

--- html_generator.py
+++ html_generator.py
@@ -1,27 +1,35 @@
 from typing import List, Text
-from htmlBuilder.attributes import Class, Href, Rel, Style,Type, Id
+from htmlBuilder.attributes import Class, Href, Rel, Style,Type, Id, Width, Height
 from htmlBuilder.tags import Html, Head, Script, Title, Body, Div, Footer, Link
 import canvas
 import pixels
 
+CANVAS_STYLE = "canvas.css"
+
 def generate_html(canvas):
     html = Html([],
         Head([],[Link([Rel("stylesheet"),Type("text/css"), Href("style.css")]),
+        Link([Rel("stylesheet"),Type("text/css"),Href(CANVAS_STYLE)]),
             Title([], "Community Paint Canvas")]
         ),
         Body([],
-            [Div([Class("row")],
-                [Div([Class("pixel"), Id("default") if px_y[pixels.GREEN_NAME] == 0.0 else Id("selected")], [])
-                for px_y in px_x]
-            )
-            for px_x in canvas]
+            [Div([Class("canvas")])
+            ]
         )
     )
 
     return html.render(pretty=True, doctype=True) # pass doctype=True to add a document declaration
 
+def generate_css(canvas):
+    with open("index/canvas.css", 'w') as f:
+        #w = "top: 0;\nleft: 0;\nright: 0;\nbottom: 0"
+        w = "width: "+str(len(canvas))+"px;\nheight: "+str(len(canvas[0]))+"px;\n"
+        s = ".canvas {\n"+w+"display: inline-block;\n"+"background-color:gold;\n"+"border: 10px solid grey;\n"+"}"
+        f.write(s)
+
 def reset_index():
     c = canvas.reset_canvas()
+    generate_css(c)
     with open("index/index.html", 'w') as f:
         s = generate_html(c)
         f.write(s)
