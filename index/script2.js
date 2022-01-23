857d29c3ae2d3d4cec574d0a3a26f4126ac72b07
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2022-01-22 21:06:57 -0800

    COUNTING PIXELS DYNAMICALLY (debounced) WORKS

--- index/script2.js
+++ index/script2.js
@@ -5,17 +5,19 @@ var app = new Vue({
       vueCanvas:null,
       painting:false,
       canvas:null,
-      ctx:null
+      ctx:null,
+      status:'',
+      totalPixels:0
     },
     methods: {
       startPainting(e) {
         this.painting = true;
-        console.log(this.painting)
+        //console.log(this.painting)
         this.draw(e)
       },
       finishedPainting() {
         this.painting = false;
-        console.log(this.painting);
+        //console.log(this.painting);
         this.ctx.beginPath()
       },
       draw(e) {
@@ -31,25 +33,81 @@ var app = new Vue({
         this.ctx.moveTo(e.offsetX,e.offsetY)
   
       },
-      getCanvasData(){
+      getTotalPixels(){
+        this.status = 'Counting...';
         let imageData = this.ctx.getImageData(0,0,this.canvas.height,this.canvas.width).data;
         let count = 0;
-        console.log(imageData.length);
+        //console.log(imageData.length);
         for(let i=0; i<imageData.length; i++){
           if(imageData[i]>0){
             count++;
           }
         }
-        console.log(count);
+        this.status = 'The total number of pixels is:'
+        //console.log(count);
+        this.totalPixels= count;
       }
-  },
-
+    },
   mounted() {
     this.canvas = document.getElementById("canvas");
-    this.ctx = canvas.getContext("2d");  
+    this.ctx = this.canvas.getContext("2d");  
   
     // Resize canvas
     this.canvas.height = window.innerHeight;
     this.canvas.width = window.innerWidth;
-  }  
+  },
+  watch:{
+    painting:function(newCount, oldCount){
+      if(this.painting){
+        this.status = 'Painting...';
+        return;
+      }
+      this.debouncedGetCanvas();
+    }
+  },
+  created:function(){
+    this.debouncedGetCanvas = _.debounce(this.getTotalPixels, 500);
+  }
+})
+
+  var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!'
+    },
+  watch: {
+    // whenever question changes, this function will run
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    // _.debounce is a function provided by lodash to limit how
+    // often a particularly expensive operation can be run.
+    // In this case, we want to limit how often we access
+    // yesno.wtf/api, waiting until the user has completely
+    // finished typing before making the ajax request. To learn
+    // more about the _.debounce function (and its cousin
+    // _.throttle), visit: https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    }
+  } 
 })
