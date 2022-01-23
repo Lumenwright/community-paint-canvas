1ff4aa63298cfd73947bba11fa3284a63a17cede
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2022-01-23 14:00:13 -0800

    firebase????????

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ import pixels
 
 #firebase auth
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, db
 
 cred = credentials.Certificate("community-paint-canvas-firebase-adminsdk-yq2qs-1f670317bb.json")
 firebase_admin.initialize_app(cred)
+
+# Get a database reference 
+ref = db.reference('server/saving_data')
+users_ref = ref.child('users')
+users_ref.set({
+    'alanisawesome': {
+        'date_of_birth': 'June 23, 1912',
+        'full_name': 'Alan Turing'
+    },
+    'gracehop': {
+        'date_of_birth': 'December 9, 1906',
+        'full_name': 'Grace Hopper'
+    }
+})
 
 #endpoints
 PIXELS = '/pixels'
